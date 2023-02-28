c27a04c65b365a1c751cccebf14534d8484212fe
Dylan Castillo <dylanjcastillo@gmail.com>
2023-03-01 00:04:47 +0100

    Minimal working version

---
+++ main.py
@@ -0,0 +1,78 @@
+import openai
+from fastapi import FastAPI
+from qdrant_client import QdrantClient
+from sentence_transformers import SentenceTransformer
+
+from config import (
+    OPENAI_API_KEY,
+    QDRANT_API_KEY,
+    QDRANT_HOST,
+    QDRANT_PORT,
+    COLLECTION_NAME,
+)
+
+openai.api_key = OPENAI_API_KEY
+
+qdrant_client = QdrantClient(
+    host=QDRANT_HOST,
+    port=QDRANT_PORT,
+    api_key=QDRANT_API_KEY,
+)
+
+retrieval_model = SentenceTransformer("msmarco-MiniLM-L-6-v3")
+
+app = FastAPI()
+
+
+def build_prompt(question: str, references: list) -> tuple[str, str]:
+    prompt = f"""
+    You're Marcus Aurelius, emperor of Rome. You're giving advice to a friend who has asked you the following question: {question}
+
+    You've selected the most relevant passages from your writings to use as source for your answer. Cite them in your answer.
+
+    References:
+    """.strip()
+
+    references_text = ""
+
+    for i, reference in enumerate(references, start=1):
+        text = reference.payload["text"].strip()
+        references_text += f"\n[{i}]: {text}"
+
+    prompt += (
+        references_text
+        + "\nHow to cite a reference: This is a citation [1]. This one too [3]. And this is sentence with many citations [2][3].\nAnswer:"
+    )
+    return prompt, references_text
+
+
+@app.get("/")
+def read_root():
+    return {
+        "message": "Make a post request to /ask to ask a question about Meditations by Marcus Aurelius"
+    }
+
+
+@app.post("/ask")
+def predict(question: str):
+    similar_docs = qdrant_client.search(
+        collection_name=COLLECTION_NAME,
+        query_vector=retrieval_model.encode(question),
+        limit=3,
+        append_payload=True,
+    )
+
+    prompt, references = build_prompt(question, similar_docs)
+
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=prompt,
+        temperature=0.2,
+        max_tokens=250,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0,
+    )
+    response["choices"][0]["text"]
+
+    return {"response": response, "references": references, "prompt": prompt}
